d2e5831f92b1b596bb2cc1ab801509da28650fcc
rvcas <x@rvcas.dev>
2023-11-20 22:18:13 -0500

    be normal for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import cloudflare from "solid-start-cloudflare-pages";
 
 export default defineConfig({
-  plugins: [solid({ adapter: cloudflare({}) })],
+  plugins: [
+    solid(
+      process.env.NODE_ENV === "PRODUCTION"
+        ? { adapter: cloudflare({}) }
+        : undefined
+    ),
+  ],
 });
