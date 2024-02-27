92cec33c8e5213db22b80d056a5a02e33c082772
rvcas <x@rvcas.dev>
2024-02-26 22:20:54 -0500

    chore: switch to sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
-import cloudflare from "solid-start-cloudflare-pages";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [
-    solid(
-      process.env.NODE_ENV === "PRODUCTION"
-        ? { adapter: cloudflare({}) }
-        : undefined
-    ),
-  ],
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
