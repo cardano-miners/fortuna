6e8cf5127d20fb73b8239a587d046f50e5a6732b
rvcas <x@rvcas.dev>
2024-03-03 00:55:13 -0500

    fix: remove more deno stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="btn btn-primary">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="btn btn-primary">Connect</h1>
+<p>Lock TUNA V1 to later claim TUNA V2</p>
