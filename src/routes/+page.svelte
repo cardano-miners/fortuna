48fbe301045636a4dbffad159ade9f57fadd3101
rvcas <x@rvcas.dev>
2024-03-16 23:15:37 -0400

    feat: improve landing page slightly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1 class="btn btn-primary">Connect</h1>
-<p>Lock TUNA V1 to later claim TUNA V2</p>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Fortuna</h1>
+      <p class="py-6">
+        The hardfork is coming. In order to prepare for the hardfork lock your $TUNA by clicking
+        this button. Locked $TUNA will be claimable on a new policy after the hardfork.
+      </p>
+      <button class="btn btn-primary">connect</button>
+    </div>
+  </div>
+</div>
