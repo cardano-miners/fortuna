9a30fef51a79bc7ef61a52ea44aefccdddd1205d
Luiz Paulo de Castro <pehcastro@gmail.com>
2024-03-18 06:35:45 -0300

    feature: added fetchWallet and refactored connectbutton / navbar with dummmy pages as components

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import ConnectButton from "./ConnectButton.svelte";
+    import fortunaLogo from '$lib/assets/fortunaLogo.png'
+
+  </script>
+  <nav class="px-6 py-3 pt-6 shadow border-b border-slate-700">
+    <div class="flex flex-col container mx-auto md:flex-row md:items-center md:justify-between">
+      <div class="flex justify-between items-center">
+        <div>
+          <a href="/"><img src={fortunaLogo} alt="logo" class="md:max-w-[200px] max-w-[20vh]"/></a>
+        </div>
+      </div>
+      <div class="md:flex flex-col md:flex-row md:-mx-4 hidden items-center  ">
+        <a href="/hardfork" class="my-1 hover:text-primary md:mx-4 md:my-0">Hardfork</a>
+        <a href="/mine" class="my-1  hover:text-primary md:mx-4 md:my-0">Mine</a>
+        <a href="/explorer" class="my-1  hover:text-primary md:mx-4 md:my-0">Explorer</a>
+        <a href="/community" class="my-1  hover:text-primary md:mx-4 md:my-0">Community</a>
+        <ConnectButton />
+      </div>
+    </div>
+  </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import '../app.css';
+  import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
+<Navbar />
 <div class="bg-base-100">
   <slot />
 </div>
