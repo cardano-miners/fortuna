9eebe7adf884e4783af0774640f24a3b08af6a0b
Luiz Paulo de Castro <pehcastro@gmail.com>
2024-03-18 06:35:45 -0300

    feature: added fetchWallet and refactored connectbutton / navbar with dummmy pages as components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { type WalletOption, type WalletApi } from '../app.d';
 
-  let open = false;
-  let walletApi: WalletApi | undefined;
-  let wallet: WalletOption | undefined;
-  let tunaBalance = 0;
-  let wallets: [string, WalletOption][] = [];
-
-  // button states
-  let locking = false;
-
-  async function connect(walletKey: string) {
-    wallet = window.cardano?.[walletKey];
-    walletApi = await wallet?.enable();
-
-    open = false;
-  }
-
-  async function lockTuna() {
-    locking = true;
-
-    if (walletApi && wallet) {
-      // TODO: lock $TUNA
-    }
-
-    setTimeout(() => {
-      locking = false;
-    }, 5000);
-  }
-
-  onMount(async () => {
-    if (typeof window.cardano !== 'undefined') {
-      wallets = Object.entries(window.cardano);
-
-      for (const [key, walletOption] of wallets) {
-        const isEnabled = await walletOption.isEnabled();
-
-        if (isEnabled) {
-          connect(key);
-          break;
-        }
-      }
-    }
-  });
 </script>
 
 <div>
@@ -115,55 +71,5 @@
         </div>
       </div>
     </div>
-    {#if walletApi && wallet}
-      <button
-        class="btn btn-primary"
-        disabled={locking}
-        on:click={(e) => {
-          e.preventDefault();
-
-          lockTuna();
-        }}>Lock $TUNA</button>
-    {:else}
-      <button
-        class="btn btn-primary"
-        on:click={(e) => {
-          open = true;
-        }}>connect</button>
-      <dialog id="my_modal_1" class="modal modal-bottom sm:modal-middle" {open}>
-        <div class="modal-box flex flex-col gap-8">
-          <h3 class="font-bold text-lg">Select a wallet</h3>
-
-          {#each wallets as [key, value]}
-            <button
-              class="btn btn-ghost flex items-center justify-center gap-2"
-              on:click={(e) => {
-                e.preventDefault();
-
-                connect(key);
-              }}>
-              <img src={value.icon} alt={value.name} class="w-6 h-6" />
-              {value.name}
-            </button>
-          {/each}
-
-          <div class="modal-action">
-            <form method="dialog">
-              <button
-                class="btn"
-                on:click={(_e) => {
-                  open = false;
-                }}>Close</button>
-            </form>
-          </div>
-        </div>
-        <form method="dialog" class="modal-backdrop">
-          <button
-            on:click={(_e) => {
-              open = false;
-            }}>close</button>
-        </form>
-      </dialog>
-    {/if}
   </div>
 </div>
---
+++ src/routes/hardfork/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+
+
+</script>
+
+
+<div> hello :P </div>
