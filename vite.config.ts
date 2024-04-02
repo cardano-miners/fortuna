67c801848af49b0ffd87b33c5cd268e60de7dcb1
rvcas <x@rvcas.dev>
2024-04-01 23:32:53 -0400

    chore: try to fix the cloudflare build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), Icons({ compiler: 'svelte' }), wasm(), topLevelAwait()],
+  plugins: [sveltekit(), Icons({ compiler: 'svelte' }), wasm(), topLevelAwait(), nodePolyfills()],
   resolve: {
     alias: {
       '@dcspark/cardano-multiplatform-lib-nodejs': '@dcspark/cardano-multiplatform-lib-browser',
