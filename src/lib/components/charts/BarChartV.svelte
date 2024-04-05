d4085624280fa0a57b9a2a123cdcd9832d82ce0a
Luiz Paulo de Castro <pehcastro@gmail.com>
2024-04-05 17:39:36 -0300

    feat: explorer page

---
+++ src/lib/components/charts/BarChartV.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import {
+    Chart,
+    Svg,
+    Axis,
+    Bars,
+    Highlight,
+    RectClipPath,
+    Tooltip,
+    TooltipItem
+  } from 'layerchart';
+  import { format } from 'date-fns';
+  import { scaleBand } from 'd3-scale';
+
+  interface data {
+    date: string;
+    value: number;
+    baseline: number;
+  }
+
+  export let data: data[] = [];
+  export let title: string = '';
+</script>
+
+<div class="card bg-base-300">
+  <div class="card-title m-4">{title}</div>
+  <div class="card-body h-[300px] p-4">
+    <Chart
+      {data}
+      x="date"
+      xScale={scaleBand().padding(0.4)}
+      y="value"
+      yDomain={[0, null]}
+      yNice={4}
+      padding={{ left: 16, bottom: 24 }}
+      tooltip={{ mode: 'band' }}>
+      <Svg label="Bar Chart">
+        <Axis placement="bottom" format={(d) => format(d, 'dd MMM ')} rule />
+        <Bars
+          radius={4}
+          strokeWidth={1}
+          class="fill-white/95  group-hover:fill-base-300 transition-colors" />
+        <Highlight
+          area={{
+            class: 'fill-black/15'
+          }}
+          bar={{
+            class: 'fill-primary',
+            strokeWidth: 1,
+            radius: 4
+          }} />
+      </Svg>
+      <Tooltip class="bg-base-300" header={(data) => format(data.date, 'eee, MMMM do')} let:data>
+        <TooltipItem label="Mined" value={data.value} />
+      </Tooltip>
+    </Chart>
+  </div>
+</div>
--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -1,7 +1,85 @@
 <script lang="ts">
   import type { PageData } from './$types';
 
+  import AreaChart from '$lib/components/charts/AreaChart.svelte';
+  import BarChartV from '$lib/components/charts/BarChartV.svelte';
+  import ArcSegments from '$lib/components/charts/ArcSegments.svelte';
+  import BlocksTable from '$lib/components/BlocksTable.svelte';
+  import MinersTable from '$lib/components/MinersTable.svelte';
+
+  // Dummy data for the charts, barebones dates and values and tunaInfo
+  import AreaChartData from '$lib/components/charts/AreaChartData.json';
+  import BarChartVData from '$lib/components/charts/BarChartVData.json';
+  import SparklineData from '$lib/components/charts/SparklineData.json';
+  import blocksTableDummy from '$lib/components/charts/blocksTableDummy.json';
+  import minersTableDummy from '$lib/components/charts/minersTableDummy.json';
+
+  // curve style for the area chart, those are the ones that can be imported  https://d3js.org/d3-shape/curve
+  import { curveStep } from 'd3-shape';
+
+  import LatestBlocks from '$lib/components/LatestBlocks.svelte';
+  import ProgressChart from '$lib/components/charts/progressChart.svelte';
+
+  // todo: responsive/mobile tailwind code
+
+  let activeTab = 'blocks';
+
   export let data: PageData;
 </script>
 
-<div class="justify-center text-center mt-[15vh] text-white text-3xl">SOON :P</div>
+<div class="mt-4 mb-[5vw] md:mx-0 mx-2">
+  <div><LatestBlocks marketData={SparklineData} /></div>
+  <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
+    <div class="col-span-1 md:col-span-4 h-full w-full">
+      <BarChartV data={BarChartVData} title="Mined Tuna/Day" />
+    </div>
+    <div>
+      <AreaChart
+        data={AreaChartData}
+        title="Estimated Hash Power"
+        curveType={curveStep}
+        placementx="top"
+        placementy="right"
+        lineClass="stroke-primary stroke-2"
+        areaClass="fill-primary/10"
+        yValue="value"
+        xValue="x"
+        hasPoints={true}
+        isHighlighted={true} />
+    </div>
+
+    <div class="col-span-1">
+      <ArcSegments value={8} segments={100} title="Block Difficult" />
+    </div>
+    <div class="col-span-1 md:col-span-2">
+      <ProgressChart blockNumbers={BarChartVData} />
+    </div>
+  </div>
+  <div>
+    <div class="pt-8 flex justify-center">
+      <span class="text-4xl font-semibold">Tuna Index</span>
+    </div>
+    <div role="tablist" class="tabs tabs-boxed tabs-lg mt-5">
+      <span
+        role="tab"
+        class="tab"
+        on:click={() => (activeTab = 'blocks')}
+        class:tab-active={activeTab === 'blocks'}>Blocks</span>
+      <span
+        role="tab"
+        class="tab"
+        on:click={() => (activeTab = 'miners')}
+        class:tab-active={activeTab === 'miners'}>Miners</span>
+    </div>
+
+    {#if activeTab === 'blocks'}
+      <div class="overflow-x-auto">
+        <BlocksTable data={blocksTableDummy} />
+      </div>
+    {:else if activeTab === 'miners'}
+      <div class="overflow-x-auto">
+        <MinersTable data={minersTableDummy} />
+      </div>
+    {/if}
+  </div>
+</div>
