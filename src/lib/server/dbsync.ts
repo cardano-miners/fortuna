6ffd09de6184c3c6c644c141e6e6fd16e42f6ab1
rvcas <x@rvcas.dev>
2024-04-05 22:26:43 -0400

    chore: idk anymore

--- src/lib/server/dbsync.ts
+++ src/lib/server/dbsync.ts
@@ -1,10 +1,25 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg-worker';
-import { Pool } from '@prisma/pg-worker';
 
-import { DATABASE_URL } from '$env/static/private';
+import { NODE_ENV } from '$env/static/private';
 
-const pool = new Pool({ connectionString: DATABASE_URL });
-const adapter = new PrismaPg(pool);
+let dbsync: PrismaClient;
 
-export const dbsync = new PrismaClient({ adapter });
+declare global {
+  // eslint-disable-next-line no-var
+  var __dbsync: PrismaClient | undefined;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (NODE_ENV === 'production') {
+  dbsync = new PrismaClient();
+} else {
+  if (!global.__dbsync) {
+    global.__dbsync = new PrismaClient({ log: ['query'] });
+  }
+
+  dbsync = global.__dbsync;
+}
+
+export { dbsync };
