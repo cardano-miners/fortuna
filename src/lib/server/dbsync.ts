9a3bdceb00b01fb936efc7c2f02bbb127170cc4a
rvcas <x@rvcas.dev>
2024-04-05 21:45:44 -0400

    chore: setup some weird db driver

--- src/lib/server/dbsync.ts
+++ src/lib/server/dbsync.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg-worker';
+import { Pool } from '@prisma/pg-worker';
 
 import { NODE_ENV, DATABASE_URL } from '$env/static/private';
 
@@ -15,16 +15,13 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 if (NODE_ENV === 'production') {
-  const pool = new pg.Pool({ connectionString: DATABASE_URL });
+  const pool = new Pool({ connectionString: DATABASE_URL });
   const adapter = new PrismaPg(pool);
 
   dbsync = new PrismaClient({ adapter });
 } else {
   if (!global.__dbsync) {
-    const pool = new pg.Pool({ connectionString: DATABASE_URL });
-    const adapter = new PrismaPg(pool);
-
-    global.__dbsync = new PrismaClient({ adapter, log: ['query'] });
+    global.__dbsync = new PrismaClient({ log: ['query'] });
   }
 
   dbsync = global.__dbsync;
