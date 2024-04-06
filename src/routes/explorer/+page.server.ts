6e2e74e61231b5a563fa6bd4ff589b525121526d
rvcas <x@rvcas.dev>
2024-04-05 20:11:12 -0400

    feat: make data usable on the frontend

--- src/routes/explorer/+page.server.ts
+++ src/routes/explorer/+page.server.ts
@@ -1,21 +1,27 @@
 import { Prisma } from '@prisma/client';
-import { PageServerLoadEvent } from './$types';
+import type { PageServerLoadEvent } from './$types';
 
 import { dbsync } from '$lib/server/dbsync';
 
+export type BlockData = {
+  block_number: number;
+  current_hash: string;
+  leading_zeros: number;
+  target_number: number;
+  epoch_time: number;
+  current_posix_time: number;
+  // only the genesis block has an undefined nonce
+  nonce?: string;
+};
+
 export async function load({ url }: PageServerLoadEvent) {
   const pageNumber = url.searchParams.get('page') ?? '1';
-  const pageLimit = url.searchParams.get('pageSize') ?? '10';
 
   let parsedPageNumber = parseInt(pageNumber);
-  let parsedPageLimit = parseInt(pageLimit);
+  const parsedPageLimit = 10;
 
-  if (isNaN(parsedPageNumber)) {
+  if (isNaN(parsedPageNumber) || parsedPageNumber < 1) {
     parsedPageNumber = 1;
-  }
-
-  if (isNaN(parsedPageLimit)) {
-    parsedPageLimit = 10;
   }
 
   const policy = Buffer.from([
@@ -35,6 +41,7 @@ export async function load({ url }: PageServerLoadEvent) {
     relationLoadStrategy: 'join',
     skip: (parsedPageNumber - 1) * parsedPageLimit,
     take: parsedPageLimit,
+    orderBy: { id: 'desc' },
     where,
     include: {
       tx_out: {
@@ -59,9 +66,24 @@ export async function load({ url }: PageServerLoadEvent) {
   const canPrevPage = parsedPageNumber > 1;
   const totalPages = Math.ceil(totalCount / parsedPageLimit);
 
-  const blocks = data.map((maTxOut) => ({
-    state: maTxOut.tx_out.datum!.value,
-    redeemer: maTxOut.tx_out.tx.redeemers[0]?.redeemer_data.value,
+  const blocks: BlockData[] = data.map((maTxOut) => ({
+    // @ts-expect-error PlutusData is typed weird
+    block_number: maTxOut.tx_out.datum!.value.fields[0].int as number,
+    // @ts-expect-error PlutusData is typed weird
+    current_hash: maTxOut.tx_out.datum!.value.fields[1].bytes as string,
+    // @ts-expect-error PlutusData is typed weird
+    leading_zeros: maTxOut.tx_out.datum!.value.fields[2].int as number,
+    // @ts-expect-error PlutusData is typed weird
+    target_number: maTxOut.tx_out.datum!.value.fields[3].int as number,
+    // @ts-expect-error PlutusData is typed weird
+    epoch_time: maTxOut.tx_out.datum!.value.fields[4].int as number,
+    // @ts-expect-error PlutusData is typed weird
+    current_posix_time: maTxOut.tx_out.datum!.value.fields[5].int as number,
+
+    // @ts-expect-error PlutusData is typed weird
+    nonce: maTxOut.tx_out.tx.redeemers[0]?.redeemer_data?.value?.fields[0].bytes as
+      | string
+      | undefined,
   }));
 
   return { blocks, canNextPage, canPrevPage, totalPages };
--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
 </script>
 
 <div class="justify-center text-center mt-[15vh] text-white text-3xl">SOON :P</div>
