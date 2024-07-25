9a835cf5ea34054673c5d4be2f7ac3c1070ada0a
rvcas <x@rvcas.dev>
2024-07-25 17:46:04 -0400

    feat: update blaze

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,5 @@
 export const prerender = false;
 export const ssr = false;
+
+import { Buffer } from 'buffer';
+globalThis.Buffer = Buffer;
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,42 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  plugins: [nodePolyfills(), sveltekit(), Icons({ compiler: 'svelte' }), wasm(), topLevelAwait()],
-  resolve: {
-    alias: {
-      '@sinclair/typebox': '@sinclair/typebox',
-      '@dcspark/cardano-multiplatform-lib-nodejs': '@dcspark/cardano-multiplatform-lib-browser',
-    },
-  },
+  plugins: [sveltekit(), Icons({ compiler: 'svelte' }), wasm()],
+
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
+
+  server: {
+    fs: {
+      allow: ['./'],
+    },
+  },
+
+  resolve: {
+    alias: {
+      'uplc-node': 'uplc-web',
+    },
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+      mainFields: ['browser'],
+    },
+    // exclude: ['@cardano-sdk/core', '@cardano-sdk/util', '@cardano-sdk/crypto']
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      dynamicRequireTargets: ['./node_modules/**/*.js'],
+    },
+    rollupOptions: {},
+  },
+  define: {
+    global: {},
   },
 });
