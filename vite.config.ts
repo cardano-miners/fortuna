790dc92a29a4aa33f6e364f91d4c9e1a5460c8d6
rvcas <x@rvcas.dev>
2024-07-25 21:53:00 -0400

    feat: polyfills?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import wasm from 'vite-plugin-wasm';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), Icons({ compiler: 'svelte' }), wasm()],
+  plugins: [sveltekit(), Icons({ compiler: 'svelte' }), nodePolyfills(), wasm()],
 
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
